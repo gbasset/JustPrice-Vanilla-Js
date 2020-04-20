24da742c7b18b332d93878f00195df755a0bee9f
gaetan <gbassetprotonmail.com>
2020-04-20 08:57:53 +0200

    new version with article and picture in array

--- script.js
+++ script.js
@@ -12,8 +12,174 @@ error.style.display = 'none'
 let nombreAleatoire = Math.floor(Math.random() * 1001);
 let coups = 0;
 let nombreChoisi;
+const arrayArticles = [
+    {
+        nom: 'Lunettes Alain FlouAffle',
+        lien: "https://drive.google.com/uc?id=1Ml6bmV00gOcK8ZR8DAlbpgNLlvL_rS5n",
+        message: 'Il est dingue AffleLLou , il est dingue !',
+        Prix: '180€'
+    },
 
+    {
+        nom: 'Baskettes NiKe ',
+        lien: "https://drive.google.com/uc?id=1FkDiwQRuh7RE3WZ3efBlp_rkVFmj43nq",
+        message: 'Il jhjhj , il est dingue !',
+        Prix: '180€'
+    },
+    {
+        nom: 'Airbus de mes deux',
+        lien: "https://drive.google.com/uc?id=1WF03epKk-umeHW_myjxjUjXam3jZlhbL",
+        message: 'Il jhjhj , il est dingue !',
+        Prix: '180€'
+    },
+    {
+        nom: 'Canard en platock',
+        lien: "https://drive.google.com/uc?id=1i5si7XpPrySWbghSrvH4BfO81QChMQGB",
+        message: 'Il jhjhj , il est dingue !',
+        Prix: '180€'
+    },
+    {
+        nom: 'NoNoss',
+        lien: "https://drive.google.com/uc?id=1JjfYvUqAj2AkC6dSp5W0-0c1YitaQ-yv",
+        message: 'Il jhjhj , il est dingue !',
+        Prix: '180€'
+    },
+    {
+        nom: 'Chaussettes',
+        lien: "https://drive.google.com/uc?id=1WjrztSjyaK6DdgjHB5Q-yT-9H0aF4EFV",
+        message: 'Il jhjhj , il est dingue !',
+        Prix: '180€'
+    },
+    {
+        nom: 'Ferrarie',
+        lien: "https://drive.google.com/uc?id=1hvAN-RP5MhaGyo0aNHsrT6X_n9WIud5W",
+        message: 'Il jhjhj , il est dingue !',
+        Prix: '180€'
+    },
+    {
+        nom: 'Arc de Triomphe',
+        lien: "https://drive.google.com/uc?id=1E7mscyceZ46-j9P_N5h1GRiiD-nR6kad",
+        message: 'Il jhjhj , il est dingue !',
+        Prix: '180€'
+    },
+    {
+        nom: 'Cullotte',
+        lien: "https://drive.google.com/uc?id=1fVwGhXk1I5a2fHZv8BIfxvbS1lTAHpJA",
+        message: 'Il jhjhj , il est dingue !',
+        Prix: '180€'
+    },
+    {
+        nom: 'PQ',
+        lien: "https://drive.google.com/uc?id=1wd65HORR5_XrpqWW9QxaDYE6XNoCbaWu",
+        message: 'Il jhjhj , il est dingue !',
+        Prix: '180€'
+    },
+    {
+        nom: 'Saucisse',
+        lien: "https://drive.google.com/uc?id=1mJXiaACS1m2Eyipi0tMaszCh8mRa_uT9",
+        message: 'Il jhjhj , il est dingue !',
+        Prix: '180€'
+    },
+    {
+        nom: 'Fusée',
+        lien: "https://drive.google.com/uc?id=1SNo4TDm9wCpdwNN8aKeCCWPXr-iBX2Hf",
+        message: 'Il jhjhj , il est dingue !',
+        Prix: '180€'
+    },
+    ,
+    {
+        nom: 'Chat de Noel',
+        lien: "https://drive.google.com/uc?id=18HKr-ei6dNobxlJn7sXA68mj63YS15Yk",
+        message: 'Il jhjhj , il est dingue !',
+        Prix: '180€'
+    },
+    {
+        nom: 'Un Nokia 3310',
+        lien: "https://drive.google.com/uc?id=191OXx3rXHheogMweXq47p0vp8jfoPh87",
+        message: 'Il jhjhj , il est dingue !',
+        Prix: '180€'
+    },
+    {
+        nom: 'Un Superbe chandelier',
+        lien: "https://drive.google.com/uc?id=1mAr3DM_C7lC6eKkFLBMFgEHP4hpdLEG9",
+        message: 'Il jhjhj , il est dingue !',
+        Prix: '180€'
+    },
+    {
+        nom: 'Un Nokia 3310',
+        lien: "https://drive.google.com/uc?id=1mAr3DM_C7lC6eKkFLBMFgEHP4hpdLEG9",
+        message: 'Il jhjhj , il est dingue !',
+        Prix: '180€'
+    },
+    {
+        nom: 'Une boite de croquettes',
+        lien: "https://drive.google.com/uc?id=1mAr3DM_C7lC6eKkFLBMFgEHP4hpdLEG9",
+        message: 'Il jhjhj , il est dingue !',
+        Prix: '180€'
+    },
+    {
+        nom: 'Une brosse à dents',
+        lien: "https://drive.google.com/uc?id=1_hsfUTSpvQLtHnTZQgC6sJ1GrVQMntIh",
+        message: 'Il jhjhj , il est dingue !',
+        Prix: '180€'
+    },
+    {
+        nom: 'Un mixeur',
+        lien: "https://drive.google.com/uc?id=1C98CNhiXY4vTe3_u45ALJSnsWKCDQIbB",
+        message: 'Il jhjhj , il est dingue !',
+        Prix: '180€'
+    },
+    {
+        nom: 'Un coupe ongles',
+        lien: "https://drive.google.com/uc?id=1GF0Y9VofkeRduLuN9zTHTqKk7X6NgxsZ",
+        message: 'Il jhjhj , il est dingue !',
+        Prix: '180€'
+    },
+    {
+        nom: 'Une tondeuse',
+        lien: "https://drive.google.com/uc?id=1pB1RmVSXmDICSQyOesqjDhAgQXVE7WIq",
+        message: 'Il jhjhj , il est dingue !',
+        Prix: '180€'
+    },
+    {
+        nom: 'Un rasoir',
+        lien: "https://drive.google.com/uc?id=1UMjjWQ3Z7ELykO0DsZ-4JQENJtM2tUPY",
+        message: 'Il jhjhj , il est dingue !',
+        Prix: '180€'
+    },
+    {
+        nom: 'Un Mackintosk',
+        lien: "https://drive.google.com/uc?id=1oNA77a7l3A2CSkexaFePraozDFwBVkNg",
+        message: 'Il jhjhj , il est dingue !',
+        Prix: '180€'
+    },
+    {
+        nom: 'Un Valise',
+        lien: "https://drive.google.com/uc?id=1GuCaiZLr3fqzV9dXz3EYzOBkqzcaVMO0",
+        message: 'Il jhjhj , il est dingue !',
+        Prix: '180€'
+    },
+
+]
+let NumArticleAleatoire = Math.floor(Math.random() * arrayArticles.length);
+let numberOfLength
+// Generer un article aléatoire
+function getAleatoireArticle(num) {
+    numberOfLength = arrayArticles[num]
+    return numberOfLength
+}
+getAleatoireArticle(NumArticleAleatoire)
+console.log("numberOfLength", numberOfLength);
+
+let imageOfArticle = document.querySelector('.imageOfArticle')
+imageOfArticle.innerHTML = `<img src=${numberOfLength.lien}>`
 let player = document.querySelector('#audioPlayer');
+
+let titleAArticle = document.querySelector('.titleAArticle')
+titleAArticle.innerHTML = `${numberOfLength.nom}`
+
+let history = document.querySelector('.history')
+history.innerHTML = `${numberOfLength.message}`
 formulaire.addEventListener('click', () => {
     player.play()
     player.volume = 0.03
